bccab9d23936832f0ae7494b72ab2ecebc73bb90
Steven Nunez <steven.nunez@gmail.com>
2014-08-25 17:23:03 -0400

    Extended Stack

--- questions.rb
+++ questions.rb
@@ -1,6 +1,5 @@
 # Create a Stack class
 #   Has methods, Push, pop, head
-# Write an extended stack class that has a function getLargest() for returning the largest element in the stack.
 
 
 class Stack
@@ -15,7 +14,9 @@ class Stack
   end
 
   def pop
+    old_head = self.head
     self.head = self.head.next
+    old_head.data
   end
 
   def head
@@ -58,5 +59,75 @@ describe Stack do
 
     stack.pop
     expect(stack.peek).to eq(5)
+  end
+
+  it "returns the popped item, when pop is called" do
+    stack = Stack.new
+    stack.push(5)
+    stack.push(6)
+
+    item = stack.pop
+    expect(item).to eq(6)
+  end
+end
+
+# Write an extended stack class that has a function get_largest for returning the largest element in the stack.
+
+# V1 Big O issues. It gets slower as the array grows
+class ExtendedStack < Stack
+  def get_largest
+    largest = 0
+    until self.peek == :terminate
+      if self.peek > largest
+        largest = self.peek
+      end
+      self.pop
+    end
+    largest
+  end
+end
+
+#V2
+class ExtendedStack < Stack
+  attr_reader :largest_stack
+  def initialize
+    @largest_stack = Stack.new
+  end
+
+  def push(item)
+    super
+    return largest_stack.push(item) if largest_stack.peek == :terminate
+
+    if item > largest_stack.peek
+      largest_stack.push(item)
+    end
+  end
+
+  def pop
+    popped = super
+    largest_stack.pop if largest_stack.peek == popped
+  end
+
+  def get_largest
+    largest_stack.peek
+  end
+end
+
+describe ExtendedStack do
+  it "returns the largest number in the stack" do
+    stack = ExtendedStack.new
+    stack.push(5)
+    stack.push(6)
+
+    expect(stack.get_largest).to eq(6)
+  end
+
+  it "responds to popping" do
+    stack = ExtendedStack.new
+    stack.push(5)
+    stack.push(6)
+    stack.pop
+
+    expect(stack.get_largest).to eq(5)
   end
 end
